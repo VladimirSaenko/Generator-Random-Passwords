05aa7b257fbc44b3279810986a878fbf026dd484
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-30 22:26:07 +0300

    del empty lines
    прост))))))

--- script/mainscript.js
+++ script/mainscript.js
@@ -3,7 +3,6 @@
 
 let alertBox = document.querySelector('.alertBox');
 
-
 function getPassword() {
     let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/@#$%&()+&<>:";
     let passwordLength = 16;
@@ -36,7 +35,7 @@ function getPassword() {
         let randomNumber = Math.floor(Math.random() * chars.length);
         password += chars[randomNumber];
     }
-
+    
     document.getElementById('password').value = password;
     let randomNumber = Math.floor(Math.random() * smiles.length);
     alertBox.innerHTML = "New Password Copied: <br>" + password + '<br><br>' + smiles[randomNumber];
